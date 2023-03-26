be168db5049d90455550b9c759d7375062a6c7cd
Muhta Nuryadi <nuryadi.muhta@gmail.com>
2023-03-26 16:47:58 +0700

    membuat fungsi tambah barang

--- config/controller.php
+++ config/controller.php
@@ -1,10 +1,12 @@
 <?php 
+
+// fungsi mengambil data
 function select($query)
 {
     global $db;
 
     $result = mysqli_query($db, $query);
-    $rows = [];
+    $rows   = [];
 
 
     while ($row = mysqli_fetch_assoc($result)) {
@@ -13,4 +15,18 @@ function select($query)
     return $rows;
 }
 
-?>
+
+// fungsi menambahkan data
+function create_barang($post){
+    global $db;
+
+    $nama   = $post['nama'];
+    $jumlah = $post['jumlah'];
+    $harga  = $post['harga'];
+
+    $query = "INSERT INTO barang VALUES (null, '$nama', '$jumlah', '$harga', CURRENT_TIMESTAMP()";
+
+    mysqli_query($db, $query);
+
+    return mysqli_affected_rows($db);
+}
