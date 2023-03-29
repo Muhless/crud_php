0608279263ac56e9a95c96f907e14f184a4d0eed
Muhta Nuryadi <nuryadi.muhta@gmail.com>
2023-03-29 16:19:30 +0700

    membuat fungsi upload file

--- config/controller.php
+++ config/controller.php
@@ -77,7 +77,12 @@ function create_mahasiswa($post)
     $jenis_kelamin      = $post['jenis_kelamin'];
     $telepon            = $post['nama'];
     $email              = $post['email'];
-    $foto               = upload_file(['foto']['name']);
+    $foto               = upload_file();
+
+    // check upload foto
+    if (!$foto) {
+        return false;
+    }
 
     // query tambah data
     $query = "INSERT INTO mahasiswa VALUES (null, '$nama', '$prodi', '$jenis_kelamin', '$telepon', '$email', '$foto');";
@@ -87,3 +92,45 @@ function create_mahasiswa($post)
     return mysqli_affected_rows($db);
 }
 
+// fungsi upload file
+function upload_file()
+{
+    global $db;
+
+    $namaFile           = $_FILES['foto']['name'];
+    $ukuranFile         = $_FILES['foto']['size'];
+    $error              = $_FILES['foto']['error'];
+    $tmpname            = $_FILES['foto']['tmp_name'];
+
+    // check file yang diupload
+    $extensifileValid   = ['jpg', 'jpeg', 'png'];
+    $extensifile        = explode('.', $namaFile);
+    $extensifile        = strtolower(end($extensifile));
+
+    // check format ekstensi file
+    if (!in_array($extensifile, $extensifileValid)) {
+        echo    "<script>
+                alert('Format File Tidak Valid');
+                document.location.href = 'tambah-mahasiswa.php';
+                </script>";
+        die();
+    }
+
+    // check ukuran file < 2 MB 
+    if ($ukuranFile > 2048000) {
+        echo    "<script>
+                alert('File Melebihi Batas Maximum 2MB');
+                document.location.href = 'tambah-mahasiswa.php';
+                </script>";
+        die();
+    }
+
+    // generate nama file baru
+    $namafileBaru = uniqid();
+    $namafileBaru .= '.';
+    $namafileBaru .= $extensifile;
+
+    // pindahkan foto hasil upload ke folder asset/img/
+    move_uploaded_file($tmpname, 'assets/img/' . $namafileBaru);
+    return $namafileBaru;
+}
